4b55bc2c519a9bce4207a71103aa90acadec664e
kkoltko <74241960+kkoltko@users.noreply.github.com>
2021-03-20 02:24:35 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,31 @@
 function initRoomsDoors () {
-    listRoomDoors = ["GP", "PL", "PL", "GL", "G", "DP", "GPL", "L", "GDP", "GDL", "P", "DPL", "L", "GD", "GD", "GP", "PL", "PL", "DL", "GD", "GP", "L", "P", "PL", "DL"]
+    listRoomDoors = [
+    "GP",
+    "PL",
+    "PL",
+    "GL",
+    "G",
+    "DP",
+    "GPL",
+    "L",
+    "GDP",
+    "GDL",
+    "P",
+    "DPL",
+    "L",
+    "GD",
+    "GD",
+    "GP",
+    "PL",
+    "PL",
+    "DL",
+    "GD",
+    "GP",
+    "L",
+    "P",
+    "PL",
+    "DL"
+    ]
 }
 function initStartAndWinRoom () {
     myWinRoom = 61
@@ -111,7 +137,33 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 function initRoomNo () {
-    listRoomNo = ["11", "12", "13", "14", "15", "21", "22", "23", "24", "25", "31", "32", "33", "34", "35", "41", "42", "43", "44", "45", "51", "52", "53", "54", "55"]
+    listRoomNo = [
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55"
+    ]
 }
 let aktualnyKierunek = 0
 let myRoomDoorsWithX = ""
